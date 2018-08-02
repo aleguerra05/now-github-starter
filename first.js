23bd60f654de6defe2cbfc830aaea13959c1f253
aleguerra <aleguerra05@gmail.com>
2018-08-02 15:05:47 -0400

    Update first.js

--- first.js
+++ first.js
@@ -2,8 +2,9 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!');
-    res.write('The date and time are currently: ' + myDateTime());
+    res.write('Hello World!\n');
+    res.write('Request url: '+ req.url+'\n');
+    res.write('The date and time are currently: ' + myDateTime() + '\n');
     res.end();
 }).listen();
 
